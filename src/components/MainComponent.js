99a5439179b8d1451b5c619ce6c93e0bdacff72e
Chunyue Ma <cma01@wesleyan.edu>
2021-01-20 14:19:31 -0500

    contact component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 
 class Main extends Component {
 
@@ -27,6 +28,7 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={ HomePage } />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
