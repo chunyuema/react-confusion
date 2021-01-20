f3e11e72a4515c53261064abd6067eb2d16f49b6
Chunyue Ma <cma01@wesleyan.edu>
2021-01-20 14:39:27 -0500

    updated home component (SPA)

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes'; 
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
@@ -14,12 +17,20 @@ class Main extends Component {
     super(props); 
     this.state = {
         dishes: DISHES, 
+        comments: COMMENTS, 
+        promotions: PROMOTIONS, 
+        leaders: LEADERS
     }; 
   }
 
   render() {
     const HomePage = () => {
-      return (<Home />);
+      return (
+        <Home 
+          dish = {this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion = {this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader = {this.state.leaders.filter((leader) => leader.featured)[0]}
+        />);
     }
 
     return (
@@ -28,7 +39,7 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={ HomePage } />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
-          <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/contactus" component={ Contact } />
           <Redirect to="/home" />
         </Switch>
         <Footer />
